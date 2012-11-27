47fce24fcf70c410a18c2a76bdab450a773810c0
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-11-27 10:54:10 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LawDigitalRepository::Application.config.secret_token = '02ac0159dcf8b0524cacf0b3e34504bed8f382042e3dc5790d9d60de064bc815d0b8a9f598177581c2fa3a53f772d556bf068e011e3f9eccc3d6427ff36f5eda'
