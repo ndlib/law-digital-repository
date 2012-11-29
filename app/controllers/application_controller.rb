472c51ddd340b6155c1411dc30125ee44cc43e91
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-11-29 12:08:39 -0500

    Adding general rescue_from behavior
    I want to capture the Fedora/SOLR based exceptions and gracefully handle
    them. Presently, I have a kludge in place, but it is adequate for now.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
+require 'common_repository_model/exceptions'
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  rescue_from CommonRepositoryModel::ObjectNotFoundError do |exception|
+    respond_to do |wants|
+      wants.json { render json: {errors: [exception.message]}, status: 404 }
+    end
+  end
 end
