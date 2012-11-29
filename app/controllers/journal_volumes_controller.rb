aee0e3abd5fd74235e46dffec7bd46dff90cc2cf
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-11-29 14:01:52 -0500

    Adding /journal_volumes.json(\?journal_id=:id)?

--- app/controllers/journal_volumes_controller.rb
+++ app/controllers/journal_volumes_controller.rb
@@ -1,6 +1,19 @@
 class JournalVolumesController < ApplicationController
   respond_to :json
 
+  def index
+    if params[:journal_id]
+      @journal_volumes = journal.volumes
+    else
+      @journal_volumes = JournalVolume.find(:all)
+    end
+    respond_with(@journal_volumes) do |format|
+      format.json {
+        render(json: {journal_volumes: @journal_volumes.collect(&:id)}.to_json)
+      }
+    end
+  end
+
   def show
     @journal_volume = JournalVolume.find(params[:id])
     respond_with(@journal_volume)
--- spec/requests/journal_volumes_spec.rb
+++ spec/requests/journal_volumes_spec.rb
@@ -2,8 +2,65 @@ require 'spec_helper'
 require 'common_repository_model/test_support'
 
 describe "Journals", type: :request do
+
+  describe "GET /journal_volumes.json" do
+
+    before(:all) do
+      @journal1 = FactoryGirl.create(:journal)
+      @journal2 = FactoryGirl.create(:journal)
+      @journal_volume1 = FactoryGirl.create(:journal_volume).tap { |obj|
+        obj.journals << @journal1
+        obj.save!
+      }
+      @journal_volume2 = FactoryGirl.create(:journal_volume).tap { |obj|
+        obj.journals << @journal1
+        obj.save!
+      }
+      @journal_volume3 = FactoryGirl.create(:journal_volume).tap { |obj|
+        obj.journals << @journal2
+        obj.save!
+      }
+    end
+
+    after(:all) do
+      @journal1.delete if @journal1
+      @journal2.delete if @journal2
+      @journal_volume1.delete if @journal_volume1
+      @journal_volume2.delete if @journal_volume2
+      @journal_volume3.delete if @journal_volume3
+    end
+
+    it 'should render JSON object of PIDs' do
+      get journal_volumes_path(format: :json)
+      response.status.should be(200)
+      json = JSON.parse(response.body)
+      root = json.fetch('journal_volumes')
+      root.should include(@journal_volume1.pid)
+      root.should include(@journal_volume2.pid)
+      root.should include(@journal_volume3.pid)
+    end
+
+    it 'should render JSON object of PIDs scoped to :journal_id' do
+      get journal_volumes_path(journal_id: @journal1.id, format: :json)
+      response.status.should be(200)
+      json = JSON.parse(response.body)
+      root = json.fetch('journal_volumes')
+      root.should include(@journal_volume1.pid)
+      root.should include(@journal_volume2.pid)
+      root.should_not include(@journal_volume3.pid)
+    end
+
+    it 'should render 404 if :journal_id is not found' do
+      get journal_volumes_path(journal_id: "-2", format: :json)
+      response.status.should be(404)
+    end
+
+  end
+
   describe "GET /journal_volumes/:pid.json" do
+
     subject { FactoryGirl.create(:journal_volume) }
+
     it 'should render JSON object' do
       get journal_volume_path(subject, format: :json)
       response.status.should be(200)
@@ -12,11 +69,16 @@ describe "Journals", type: :request do
         json.fetch('journal_volume').fetch('pid')
       }.to_not raise_error(KeyError)
     end
+
   end
+
   describe "POST /journals.json" do
+
     describe 'with valid data' do
+
       let(:journal) { FactoryGirl.create(:journal) }
       let(:parameters) { FactoryGirl.attributes_for(:journal_volume) }
+
       it "should create a new item" do
         expect {
           post(
@@ -29,9 +91,12 @@ describe "Journals", type: :request do
         JournalVolume.find(assigns(:journal_volume).pid).journals.
           should == [journal]
       end
+
     end
     describe 'without :journal_id' do
+
       let(:parameters) { FactoryGirl.attributes_for(:invalid_journal_volume) }
+
       it "should NOT create a new item" do
         expect {
           post(
@@ -41,21 +106,32 @@ describe "Journals", type: :request do
         }.to change { JournalVolume.count }.by(0)
         response.status.should be(404)
       end
+
     end
+
   end
+
   describe "PUT /journals.json" do
+
     before(:each) do
       @journal_volume = FactoryGirl.create(:journal_volume)
     end
+
     after(:each) do
       @journal_volume.delete rescue true
     end
+
     describe 'with valid data' do
+
       let(:parameters) { FactoryGirl.attributes_for(:journal_volume) }
+
       it 'should update an existing item' do
         put journal_volume_path(@journal_volume, format: :json), journal_volume: parameters
         response.status.should be(204)
       end
+
     end
+
   end
+
 end
